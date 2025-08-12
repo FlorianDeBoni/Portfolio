67dc33ee7195949346e784af123ee16e44be933a
FlorianDeBoni <florian.de.boni@gmail.com>
2025-08-12 12:02:35 +0200

    adding working basic router

---
+++ portfolio/src/router.js
@@ -0,0 +1,15 @@
+import { createWebHistory, createRouter } from 'vue-router'
+import HomeView from './components/Home.vue'
+import AboutView from './components/About.vue'
+
+const routes = [
+  { path: '/', component: HomeView },
+  { path: '/about', component: AboutView },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+export default router
